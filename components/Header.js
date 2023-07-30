1ad134286143220123f89e2fc50d830be1874915
DAndi5 <59227557+DAndi5@users.noreply.github.com>
2023-07-31 00:41:28 +0300

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+// 'useClient'
+// import Link from "next/link"
+import NavBar from "@/components/NavBar";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <NavBar/>
+        </header>
+    )
+}
+export default Header
